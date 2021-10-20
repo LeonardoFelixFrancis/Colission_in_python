7315b9156bad90433a733811915c992dc87bf3bd
LeonardoFelixFrancis <65123915+LeonardoFelixFrancis@users.noreply.github.com>
2021-10-20 00:40:12 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,3 @@
+arr = [0,1,2,3,4,5,6]
+x = arr.remove(2)
+print(x)
---
+++ tile.py
@@ -0,0 +1,6 @@
+class tile():
+    def __init__(self, xpos, ypos, height, width):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.width = width
+        self.height = height
